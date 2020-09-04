ac16222472dc933496bc2943f7a729a76042a6c0
Bright Kanyange <brightkanyange@outlook.com>
2020-09-04 17:23:07 +0300

    Added wagebill

--- projects/urls.py
+++ projects/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from .views.project_views import *
 from .views.sites_views import *
+import projects.views.worker_views  as worker_views
 
-urlpatterns = [
-    path('', index_page, name='index_page'),
-    path('projects/', projects_page_view, name='manage_projects'),
-    path('project_details/<int:project_id>/', project_details_view, name='project_details'),
-    path('project_settings/', projects_settings_view, name='project_settings'),
-    path('project/<int:project_id>/sites/', sites_page_view, name='manage_sites'),
-    path('project/<int:project_id>/site/<int:site_id>/', site_details_view, name='site_details'),
+worker_urls = [
+    path('manage_workers/', worker_views.manage_workers_page, name='manage_workers_page'),
+    path('delete_worker/<int:id>/', worker_views.delete_worker, name='delete_worker'),
 ]
+urlpatterns = [
+                  path('', index_page, name='index_page'),
+                  path('projects/', projects_page_view, name='manage_projects'),
+                  path('project_details/<int:project_id>/', project_details_view, name='project_details'),
+                  path('project_settings/', projects_settings_view, name='project_settings'),
+                  path('project/<int:project_id>/sites/', sites_page_view, name='manage_sites'),
+                  path('project/<int:project_id>/site/<int:site_id>/', site_details_view, name='site_details')
+              ] + worker_urls
