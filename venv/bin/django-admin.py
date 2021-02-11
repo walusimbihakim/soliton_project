a2809e101859a4541625644aa0e20db167145a6b
Bright Kanyange <brightkanyange@outlook.com>
2021-02-11 13:24:08 +0300

    Untrack pipfiles

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bright/Documents/Projects/soliton_project_container/soliton_project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
