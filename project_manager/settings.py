efc874ba789f25183f498f9626f68617c7c6abbf
Bright Kanyange <brightkanyange@outlook.com>
2021-02-17 12:36:08 +0300

    changed the list

--- project_manager/settings.py
+++ project_manager/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get("DEBUG") == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(",")
+ALLOWED_HOSTS = ['.localhost', '.herokuapp.com', '127.0.0.1']
 PROJECT_APPS = [
     'clients',
     'projects',
