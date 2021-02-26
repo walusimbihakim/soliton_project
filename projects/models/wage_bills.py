3be164b5ca7849f7d0f13823b13175381b704f11
Bright Kanyange <brightkanyange@outlook.com>
2021-02-26 14:42:19 +0300

    Added CSV on wagebill

--- projects/models/wage_bills.py
+++ projects/models/wage_bills.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.urls import reverse
 import calendar
 
-class WageBill(models.Model):
 
+class WageBill(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     status = models.CharField(max_length=50, default="Current")
