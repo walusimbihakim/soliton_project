0ba9fdd37395f694dc2b547e226aab10712cf735
Bright Kanyange <brightkanyange@outlook.com>
2021-02-28 05:24:38 +0300

    Changed the order of the commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate --no-input
+
 
 gunicorn project_manager.wsgi:application --bind 0.0.0.0:8000
 
