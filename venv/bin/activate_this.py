e4d0ba0979d39ef6dcf88f0d9047508527bf35bf
Bright Kanyange <brightkanyange@outlook.com>
2021-02-28 04:30:28 +0300

    Removed unwanted files

--- venv/bin/activate_this.py
+++ venv/bin/activate_this.py
@@ -19,7 +19,7 @@ base = bin_dir[: -len("bin") - 1]  # strip away the bin part from the __file__, 
 
 # prepend bin to PATH (this file is inside the bin directory)
 os.environ["PATH"] = os.pathsep.join([bin_dir] + os.environ.get("PATH", "").split(os.pathsep))
-os.environ["VIRTUAL_ENV"] = base  # virtual ..env is right above bin directory
+os.environ["VIRTUAL_ENV"] = base  # virtual .env is right above bin directory
 
 # add the virtual environments libraries to the host python import mechanism
 prev_length = len(sys.path)
