0b6d10f8123f50059dceb0dfe39a2759a061c848
Bright Kanyange <brightkanyange@outlook.com>
2021-03-06 14:58:50 +0300

    Deploying to heroku

--- clients/forms.py
+++ clients/forms.py
@@ -8,27 +8,9 @@ from .models import *
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
-        fields = ("company_name", "address", "email", "contact",
-                  "website", "contact_person", "profile_pic")
+        fields = "__all__"
 
     def __init__(self, *args, **kwargs):
         super(ClientForm, self).__init__(*args, **kwargs)
 
         self.helper = FormHelper()
-        self.helper.layout = Layout(
-            TabHolder(
-                Tab(
-                    'Basic Information',
-                    Field('company_name', css_class="col-md-12"),
-                    Field('address', wrapper_class="col-md-12"),
-                    Field('email', wrapper_class="col-md-6"),
-                    Field('contact', wrapper_class="col-md-6"),
-                    Field('website', wrapper_class="col-md-6"),
-                    Field('contact_person', wrapper_class="col-md-6"),
-                    Field('profile_pic', wrapper_class="col-md-12"),
-                ),
-                Tab(
-                    'Settings',
-                )
-            )
-        )
--- clients/views.py
+++ clients/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.contrib import messages
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 
 from projects.decorators.auth_decorators import project_manager_required
 from .selectors import *
@@ -14,6 +14,7 @@ def clients_page_view(request):
     form = ClientForm()
     if request.method == "POST":
         form = ClientForm(request.POST, request.FILES)
+        print(f"The form is valid:{form.is_valid()}")
         if form.is_valid():
             form.save()
             redirect('manage_clients')
@@ -45,12 +46,8 @@ def add_client_contact(request):
         return JsonResponse({'success': True})
 
 
-def delete_client(request):
-    client_id = request.POST.get('id')
-
+def delete_client(request, client_id):
     client = get_client(client_id)
-
     client.delete()
     messages.success(request, f'Client {client} has been deleted Successfully')
-
-    return JsonResponse({'success': True})
+    return HttpResponseRedirect(reverse(clients_page_view))
