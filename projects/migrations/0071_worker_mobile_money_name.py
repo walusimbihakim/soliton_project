30709215e1b3d38192786c0ef6bb0f00e64cfdb6
Walusimbi Hakim <hakim.walusimbi@soliton.co.ug>
2021-03-09 15:42:10 +0300

    added mobile money name

--- projects/migrations/0071_worker_mobile_money_name.py
+++ projects/migrations/0071_worker_mobile_money_name.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='worker',
             name='mobile_money_name',
-            field=models.CharField(default=None, max_length=50),
+            field=models.CharField(default=None, max_length=50, null=True),
             preserve_default=False,
         ),
     ]
