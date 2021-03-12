5f5617d7cad7b44b7733064aa59b8643480d9b29
Walusimbi Hakim <hakim.walusimbi@soliton.co.ug>
2021-03-12 20:02:10 +0300

    Done Aggregating Complaint

--- projects/templatetags/wage_bill_extras.py
+++ projects/templatetags/wage_bill_extras.py
@@ -1,6 +1,8 @@
 from django import template
-import projects.models.workers as workers
-from projects.selectors.wage_bill_selectors import get_airtel_money_withdraw_charge
+
+from projects.selectors.wage_bill_selectors import get_airtel_money_withdraw_charge, get_current_wage_bill
+from projects.selectors.workers import get_worker
+from projects.selectors.complaints import get_worker_complaints
 
 register = template.Library()
 
@@ -20,10 +22,22 @@ def wage_total_filter(amount):
 
 
 @register.filter
-def get_worker(worker_id, option):
-    worker = workers.Worker.objects.get(pk=worker_id)
+def get_worker_info(worker_id, option):
+    worker = get_worker(worker_id)
 
     if option == 1:
         return worker.name
     elif option == 2:
         return worker.mobile_money_number
+
+@register.filter
+def total_payment(worker_id, payment):
+    worker = get_worker(worker_id)
+
+    wage_bill = get_current_wage_bill()
+
+    complaint = get_worker_complaints(worker,wage_bill)
+
+    
+    return (payment+complaint["payment"])    
+
