87fbe556b9bb77ad146b49cb94650a0090dd0f9e
Bright Kanyange <brightkanyange@outlook.com>
2021-03-17 10:09:01 +0300

    Registered projects on admin panel

--- projects/admin.py
+++ projects/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 from projects.models import Worker, Survey, ExecutionScope, Team, FieldManager, \
-    PIPTeam, WageSheet, Wage, Activity
+    PIPTeam, WageSheet, Wage, Activity, Project
 from projects.models.boqs import BOQ, MaterialBOQItem, ServiceBOQItem
 from projects.models.client_activity_rates import ClientActivityRate
 from projects.models.materials import Material
 from projects.models.segments import Segment
 from projects.models.users import User
 
+admin.site.register(Project)
 admin.site.register(Worker)
 admin.site.register(Survey)
 admin.site.register(BOQ)
