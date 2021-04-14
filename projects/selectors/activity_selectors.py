e5dca40962c7342b57efc20ad5762f5f909b68c1
Bright Kanyange <brightkanyange@outlook.com>
2021-04-14 14:49:04 +0300

    Arranged activities alphabetically

--- projects/selectors/activity_selectors.py
+++ projects/selectors/activity_selectors.py
@@ -1,7 +1,9 @@
 from projects.models.activity_list import Activity
 
+
 def get_activities():
-    return Activity.objects.all()
+    return Activity.objects.order_by("code")  # Arrange activities alphabetically
+
 
 def get_activity(id):
     return Activity.objects.get(pk=id)
