c405e32cd1bd47d190934726e251c820bab23191
Bright Kanyange <brightkanyange@outlook.com>
2021-04-19 15:15:13 +0300

    Done working on segments

--- projects/forms/survey_form.py
+++ projects/forms/survey_form.py
@@ -1,38 +1,26 @@
-from django.forms import ModelForm, HiddenInput, DateInput, ChoiceField, Select
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout
-from crispy_forms.bootstrap import TabHolder, Tab, Field
-
+from django.forms import ModelForm, HiddenInput, DateInput
 from projects.models.survey import Survey
 from projects.models.project_settings import UnitOfMeasure
 
 units_of_measure = UnitOfMeasure.objects.all()
 
-class SurveyForm(ModelForm):
-    # forms.ChoiceField(, choices=[CHOICES], required=False) FormsTools.EmployeesToTuples(
-    
-    # units_of_measure = UnitOfMeasure.objects.all()
 
+class SurveyForm(ModelForm):
     class Meta:
         model = Survey
         fields = (
             "project",
             "survey_date",
-            "segmate",
-            "survey_type", 
+            "survey_type",
             "scope",
-            "unit_of_measure", 
-            "coordinates_lat", 
+            "unit_of_measure",
+            "coordinates_lat",
             "coordinates_long"
         )
         widgets = {
-            'survey_date': DateInput(format=('%m/%d/%Y'), attrs={'type': 'date'}),
-            # 'unit_of_measure': ChoiceField(choices=list(units_of_measure))
+            'survey_date': DateInput(format='%m/%d/%Y', attrs={'type': 'date'}),
         }
 
     def __init__(self, *args, **kwargs):
         super(SurveyForm, self).__init__(*args, **kwargs)
-
         self.fields['project'].widget = HiddenInput()
-
-       
