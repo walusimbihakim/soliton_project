b552a44e847d9658994b5f8134949661487a38de
Walusimbi Hakim <hakim.walusimbi@soliton.co.ug>
2021-04-21 16:32:22 +0300

    Reference unit measures to activity list

--- projects/models/project_settings.py
+++ projects/models/project_settings.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class UnitOfMeasure(models.Model):
-    unit_of_measure = models.CharField(max_length=50)
-    description = models.TextField()
+    unit_of_measure = models.CharField(max_length=50, default="Meter")
+    description = models.TextField(null=True)
 
     def __str__(self):
         return self.unit_of_measure
