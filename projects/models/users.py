5d08683cd34811219637bd74324eec522dc858a1
Bright Kanyange <brightkanyange@outlook.com>
2021-04-27 10:45:31 +0300

    Re arranged user and activity tables

--- projects/models/users.py
+++ projects/models/users.py
@@ -45,7 +45,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
-
     USERNAME_FIELD = 'username'
     objects = UserManager()
 
