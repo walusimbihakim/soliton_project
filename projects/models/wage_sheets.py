3512323458448671a8190ed49e251726519c56eb
Bright Kanyange <brightkanyange@outlook.com>
2021-06-16 11:43:12 +0300

    ordered by supervisor name

--- projects/models/wage_sheets.py
+++ projects/models/wage_sheets.py
@@ -53,7 +53,7 @@ class WageSheet(models.Model):
         return (self.total_wages + self.total_complaints) - self.total_deductions
 
     class Meta:
-        ordering = ("date",)
+        ordering = ("supervisor_user__first_name",)
         unique_together = ('supervisor_user', 'field_manager_user', 'date', 'wage_bill')
 
     def __str__(self):
