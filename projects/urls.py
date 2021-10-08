fbfe6115f2a5a0a6b712c6fffcbffc15d9a17df3
Walusimbi Hakim <hakim.walusimbi@soliton.co.ug>
2021-10-08 12:13:48 +0300

    workers by gender

--- projects/urls.py
+++ projects/urls.py
@@ -277,9 +277,7 @@ wage_bill_urls = [
     path('manager_payment_breakdown/<int:wage_bill_id>/<int:manager>/',
          wage_bill_views.wage_bill_manager_payment_breakdown,
          name="manager_payment_breakdown"),
-     path('wage_bill_activity_summary/<int:wage_bill_id>/',
-     wage_bill_views.wage_bill_activity_summary, name="wage_bill_activity_summary"),
-    path('payments_dashboard/<int:wage_bill_id>/', wage_bill_views.payments_dashboard, name="payments_dashboard"),
+     path('payments_dashboard/<int:wage_bill_id>/', wage_bill_views.payments_dashboard, name="payments_dashboard"),
     path('payment_stats_excel/<int:wage_bill_id>/', wage_bill_views.payment_stats_excel, name="payment_stats_excel"),
 
 ]
